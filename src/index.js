a8ec557d98fa2973463856073f0d036c25741951
jihaibo <17827855+json-HB@users.noreply.github.com>
2019-11-25 22:23:50 +0800

    upate

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import './index.css';
-import UseState from './use-state'; // useState
-import SearchResult from './search-result'; // 请求数据
-import UseEffect from './use-effect'; // useEffect代替生命周期函数
-import UseContext from './use-context'; // useContext 父组件向子组件传值
-import ReducerDemo from './use-reducer'; // useReducer
-import TextRedux from './test-redux/t-redux'; // useReducer模拟redux状态共享
-import UseMemo from './use-memo'; // useMemo一种性能优化的手段
-import Useref from './use-ref'; // useRef获取dom元素
-import UseWinSize from './custom'; // 一个自定义的hook函数，用于获取浏览器窗口尺寸
+import UseState from "./use-state"; // useState
+import SearchResult from "./search-result"; // 请求数据
+import UseEffect from "./use-effect"; // useEffect代替生命周期函数
+import UseContext from "./use-context"; // useContext 父组件向子组件传值
+import ReducerDemo from "./use-reducer"; // useReducer
+import TextRedux from "./test-redux/t-redux"; // useReducer模拟redux状态共享
+import UseMemo from "./use-memo"; // useMemo一种性能优化的手段
+import Useref from "./use-ref"; // useRef获取dom元素
+import UseWinSize from "./custom"; // 一个自定义的hook函数，用于获取浏览器窗口尺寸
 
-ReactDOM.render(<SearchResult />, document.getElementById('root'));
+ReactDOM.render(<SearchResult />, document.getElementById("root"));
--- src/search-result.js
+++ src/search-result.js
@@ -1,72 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import { Table } from 'antd';
-import axios from 'axios';
-import moment from 'moment';
-
-
+import React, { useState, useEffect } from "react";
+import { Table, Input, Row, Col } from "antd";
+import axios from "axios";
+import moment from "moment";
 
 const columns = [
   {
-    title: '序号',
-    dataIndex: 'created_at',
-    key: 'created_at',
+    title: "序号",
+    dataIndex: "created_at",
+    key: "order",
     render: (text, record, i) => {
       return i + 1;
     }
   },
   {
-    title: '创建时间',
-    dataIndex: 'created_at',
-    key: 'created_at',
-    render: (text) => {
-      return moment(text).format('YYYY-MM-DD');
+    title: "创建时间",
+    dataIndex: "created_at",
+    key: "created_at",
+    render: text => {
+      return moment(text).format("YYYY-MM-DD");
     }
   },
   {
-    title: '标题',
-    dataIndex: 'title',
-    key: 'title',
+    title: "标题",
+    dataIndex: "title",
+    key: "title"
   },
   {
-    title: '住址',
-    dataIndex: 'title',
-    key: 'title',
-  },  
-  {
-    title: '观看数',
-    dataIndex: 'points',
-    key: 'points',
+    title: "住址",
+    dataIndex: "title",
+    key: "address"
   },
+  {
+    title: "观看数",
+    dataIndex: "points",
+    key: "points"
+  }
 ];
-
 
 let timer = null;
 function SearchResults() {
-  const [data, setData] = useState({ hits: []});
-  const [query, setQuery] = useState('react');
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("react");
   const [loading, setLoad] = useState(false); // 默认页面loading
 
-  // 
+  //
   useEffect(() => {
     // 请求数据异步方法
     async function fetchData() {
-      console.log('22');
+      console.log("22");
       setLoad(true); // 改变loading为true
-      const result = await axios('https://hn.algolia.com/api/v1/search?query=' + query);
+      const result = await axios(
+        "https://hn.algolia.com/api/v1/search?query=" + query
+      );
       setLoad(false); // 请求完数据将loading改为 false
       setData(result.data);
     }
     // 请求数据
     if (timer) {
-      clearTimeout(timer)
+      clearTimeout(timer);
     }
     timer = setTimeout(() => {
       fetchData();
-    }, 300)
-    return () => { 
-      console.log(loading, '解绑');
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, 300);
+    return () => {
+      console.log(loading, "解绑");
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query]);
   // 仅在query更新时更新。
   // 如果业务需要这边数组传进去的是一个对象，那么页面会一直执行useEffect，
@@ -74,7 +73,18 @@ function SearchResults() {
 
   return (
     <div className="search-result">
-      查询：<input value={query} onChange={e => setQuery(e.target.value)} className="search" />
+      查询：
+      <Row>
+        <Col span="6">
+          <Input
+            placeholder="Please input"
+            onChange={e => {
+              setQuery(e.target.value);
+            }}
+          />
+        </Col>
+      </Row>
+      <br />
       <div className="table-box">
         <Table
           dataSource={data.hits || []}
@@ -84,7 +94,7 @@ function SearchResults() {
         />
       </div>
     </div>
-  )
+  );
 }
 
 export default SearchResults;
